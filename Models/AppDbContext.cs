dc5f9769d6cf23136a2f0cd015d0c0822262fbb7
asifjack <mdasifwr@gmail.com>
2022-06-21 04:22:17 +0530

    Add project files.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminAppIden.Models
+{
+    public class AppDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
